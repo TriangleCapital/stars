9c29816fea0f0d493842442bd5a0812e41b0053f
unknown <aronilie.code@gmail.com>
2025-03-02 21:49:26 +0100

    Updated

--- src/modules/leads-form-demo/index.tsx
+++ src/modules/leads-form-demo/index.tsx
@@ -11,7 +11,7 @@ const root = createRoot(rootElement!);
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<LeadsForm />} />
+      <Route path="/solicitar-informacion" element={<LeadsForm />} />
     </Routes>
   </BrowserRouter>
 );
--- src/shared/utils/urls.ts
+++ src/shared/utils/urls.ts
@@ -1,5 +1,5 @@
 const isTest = false;
 
-const prodApiUrl = 'https://api.autotrafic.es';
+const prodApiUrl = 'https://api.novafinance.es';
 const localApiUrl = 'http://localhost:3300';
 export const BACKEND_API_URL = isTest ? localApiUrl : prodApiUrl;
