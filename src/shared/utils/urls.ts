092ea59cdffd44222316342dd52222846577f90a
unknown <aronilie.code@gmail.com>
2025-03-05 18:41:22 +0100

    Updated backend url

--- src/shared/utils/urls.ts
+++ src/shared/utils/urls.ts
@@ -1,5 +1,5 @@
 const isTest = false;
 
-const prodApiUrl = 'https://www.api.novafinance.es/';
+const prodApiUrl = 'https://www.api.novafinance.es';
 const localApiUrl = 'http://localhost:3100';
 export const BACKEND_API_URL = isTest ? localApiUrl : prodApiUrl;
