b11e0483ad7c05504474dda2a3dd19dbb63b207f
unknown <aronilie.code@gmail.com>
2025-12-06 07:33:37 +0100

    Updated urls

--- webpack.config.ts
+++ webpack.config.ts
@@ -6,11 +6,11 @@ import Dotenv from 'dotenv-webpack';
 
 module.exports = (env: any, argv: any) => {
   let entry = './src/modules/manychat-lobby/index.tsx';
-  let publicPath = 'https://leads.trianglecapital.es/';
+  let publicPath = 'https://manychat.trianglecapital.es/';
 
   if (env.leads_form_demo) {
     entry = './src/modules/leads-form-demo/index.tsx';
-    publicPath = 'https://novafinance.es/';
+    publicPath = 'https://leads.trianglecapital.es/';
   } else if (env.solvia_drive_excel) {
     entry = './src/modules/solvia-drive-excel/index.tsx';
     publicPath = 'https://solvia.trianglecapital.es/';
